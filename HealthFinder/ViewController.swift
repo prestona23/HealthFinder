e0d7f963862d6586b6113b40d3cda9b5158f1645
Aaron Preston <preston_aaron@hotmail.com>
2019-10-06 22:49:04 -0400

    Initial commit.

---
+++ HealthFinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HealthFinder
+//
+//  Created by Aaron Preston on 10/6/19.
+//  Copyright © 2019 Aaron Preston. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
