cb21f8adbf711415602598c2458455ba54fc61f3
Aaron Preston <preston_aaron@hotmail.com>
2019-10-07 22:23:45 -0400

    Fixing some warnings and minor bugs.

--- HealthFinder/Cells/ResourceTableViewCell.swift
+++ HealthFinder/Cells/ResourceTableViewCell.swift
@@ -18,10 +18,9 @@ class ResourceTableViewCell: UITableViewCell {
         // Initialization code
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    override func prepareForReuse() {
+        resourceImageView.image = nil
+        resourceTitleLabel.text = nil
     }
     
     func configureCell(forResource resource: Resource?) {
